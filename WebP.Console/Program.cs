b9f5383752159e00a6a43298d6075a768dcb63c3
Ming Lu <mclu@asicentral.com>
2019-11-10 20:38:41 -0500

    added allow alpha channel.

--- WebP.Console/Program.cs
+++ WebP.Console/Program.cs
@@ -17,10 +17,10 @@ namespace WebP.ConsoleApp
 
         static void Main(string[] args)
         {
-            //args = new List<string>
-            //{
-            //    "-iC:\\temp\\tiny", "-f91325746.png", "-oC:\\temp\\out-image-tiny-110m42", "-s95", "-t100"
-            //}.ToArray();
+            args = new List<string>
+            {
+                "-iC:\\temp\\tiny", "-f*.jpg|*.png" , "-oC:\\temp\\diff", "-s99", "-t70", "-afalse"
+            }.ToArray();
             //if (args.Length == 0)
             //{
             //    Console.WriteLine("Please execute the run-me.bat file instead this executable directly.");
@@ -73,7 +73,7 @@ namespace WebP.ConsoleApp
                 Directory.CreateDirectory(outputPath);
 
                 //CreateWebPImage(Image.FromFile(a), quality, newFilePath);
-                var result = CreateWebPImage(a.FullName, o.StartingQuality, o.Tolerance);
+                var result = CreateWebPImage(a.FullName, o.StartingQuality, o.Tolerance, o.Lossless || o.StartingQuality == 100 ? 100 : 99, o.AllowAlphaChannel);
                 newTotalSize += result.Item3.Length;
                 var reduction = (a.Length - result.Item3.Length) * 100 / a.Length;
                 if (reduction < 0)
@@ -96,23 +96,23 @@ namespace WebP.ConsoleApp
             });
         }
 
-        public static Tuple<int, int, byte[]> CreateWebPImage(string inPath, int quality, int tolerance)
+        public static Tuple<int, int, byte[]> CreateWebPImage(string inPath, int quality, int tolerance, int maxQuality, bool allowAlphaChannel)
         {
             var bytes = File.ReadAllBytes(inPath);
             using(var ms = new MemoryStream(bytes))
             using (var oriImage = Image.FromStream(ms))
             {
-                if (HasTranparency(oriImage))
+                if (!allowAlphaChannel && HasTranparency(oriImage))
                 {
                     using (var referenceImage = RemoveTransparency(oriImage))
                     {
 
-                        return CreateWebPImage(bytes, referenceImage, quality, tolerance);
+                        return CreateWebPImage(bytes, referenceImage, quality, tolerance, maxQuality, allowAlphaChannel);
                     }
                 }
                 else
                 {
-                    return CreateWebPImage(bytes, oriImage, quality, tolerance);
+                    return CreateWebPImage(bytes, oriImage, quality, tolerance, maxQuality, allowAlphaChannel);
                 }
             }
 
@@ -144,26 +144,56 @@ namespace WebP.ConsoleApp
             return false;
         }
 
-        public static Tuple<int, int, byte[]> CreateWebPImage(byte[] bytes, Image referenceImage, int quality, int tolerance)
+        public static Tuple<int, int, byte[]> CreateWebPImage(byte[] bytes, Image referenceImage, int quality, int tolerance, int maxQuality, bool allowAlphaChannel)
         {
             //var bitDepth = FormatUtilities.GetSupportedBitDepth(referenceImage.PixelFormat);
-            var webp = GetWebP(bytes, quality);
+            byte[] webp = null;
+            byte[] previousWebp = null;
+            int previousQuality = 0;
             var diff = 1000;
+            //if (quality == 100)
+            //{
+            //    webp = GetWebP(bytes, quality);
+            //    using (var ms = new MemoryStream(webp))
+            //    {
+            //        using (var temp = new WebPFormat().Load(ms))
+            //            diff = GetDiff(referenceImage, temp, tolerance);
+            //    }
+            //    return new Tuple<int, int, byte[]>(diff, quality, webp);
+            //}
 
-            while (diff > tolerance && quality < 100)
+
+            while (diff > tolerance && quality <= maxQuality)
             {
+                webp = GetWebP(bytes, quality, allowAlphaChannel);
+                if(webp.Length >= bytes.Length && previousWebp != null && webp.Length > previousWebp.Length)
+                {
+                    using (var ms = new MemoryStream(previousWebp))
+                    {
+                        using (var temp = new WebPFormat().Load(ms))
+                            diff = GetDiff(referenceImage, temp, tolerance, 99);
+                    }
+                    webp = previousWebp;
+                    quality = previousQuality;
+                    break;
+                }
+
+                previousWebp = webp;
+                previousQuality = quality;
+                using(var ms = new MemoryStream(webp))
+                {
+                    using (var temp = new WebPFormat().Load(ms))
+                        diff = quality == 100 ? 0 : GetDiff(referenceImage, temp, tolerance, quality);
+                }
+
+                if (quality == maxQuality) break;
+
                 if (quality < 70) quality += 10;
                 else if (quality >= 70 && quality < 90) quality += 5;
                 else if (quality >= 90 && quality < 96) quality += 2;
                 else quality++;
-                webp = GetWebP(bytes, quality);
-                using(var ms = new MemoryStream(webp))
-                {
-                    using (var temp = new WebPFormat().Load(ms))
-                        diff = GetDiff(referenceImage, temp, tolerance);
-                }
             }
-            return new Tuple<int, int, byte[]>(diff == 1000? 0: diff, quality, webp);
+            return new Tuple<int, int, byte[]>(diff, quality, webp);
         }
 
         //public static byte[] GetWebP(Image image, int quality)
@@ -182,8 +212,13 @@ namespace WebP.ConsoleApp
         //    }
         //}
 
-        public static byte[] GetWebP(byte[] bytes, int quality)
+        public static byte[] GetWebP(byte[] bytes, int quality, bool allowAlphaChannel)
         {
+            //using (var webp = new WebPWrapper.WebP())
+            //{
+            //    return webp.EncodeLossless((Bitmap)Image.FromStream(new MemoryStream(bytes)), 9);
+            //}
+
             using (var memoryStream = new MemoryStream())
             {
                 using (var imageFactory = new ImageFactory())
@@ -191,7 +226,9 @@ namespace WebP.ConsoleApp
                     imageFactory.Load(bytes);
 
                     imageFactory.Quality = quality;
-                    imageFactory.BackgroundColor(Color.White);
+                    //imageFactory.Alpha(0);
+                    if(!allowAlphaChannel)
+                        imageFactory.BackgroundColor(Color.White);
                     imageFactory.Save(memoryStream, new WebPFormat());
                 }
                 // new WebPFormat().Save(memoryStream, Image.FromFile("C:\\temp\\tiny\\91325746.png"), bitDepth, quality);
@@ -199,10 +236,72 @@ namespace WebP.ConsoleApp
             }
         }
 
-        public static int GetDiff(Image img1, Image img2, int tolerance)
+        public static int GetDiff(Image img1, Image img2, int tolerance, int quality)
         {
             var bm1 = (Bitmap)img1;
             var bm2 = (Bitmap)img2;
+            var max_diff = 0;
+
+            for (var x = 0; x < img1.Width; x++)
+            {
+                for (var y = 0; y < img1.Height; y++)
+                {
+                    // Calculate the pixels' difference.
+                    var color1 = bm1.GetPixel(x, y);
+                    var color2 = bm2.GetPixel(x, y);
+                    var diff =
+                        Math.Abs(color1.R - color2.R) +
+                        Math.Abs(color1.G - color2.G) +
+                        Math.Abs(color1.B - color2.B);
+
+                    if (diff > tolerance && quality != 99) return diff;
+                    if (diff > max_diff) max_diff = diff;
+                }
+            }
+
+            return max_diff;
+        }
+
+        public static Bitmap MakeGrayscale3(Bitmap original)
+        {
+            //create a blank bitmap the same size as original
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+
+            //get a graphics object from the new image
+            Graphics g = Graphics.FromImage(newBitmap);
+
+            //create the grayscale ColorMatrix
+            ColorMatrix colorMatrix = new ColorMatrix(
+               new float[][]
+               {
+         new float[] {.3f, .3f, .3f, 0, 0},
+         new float[] {.59f, .59f, .59f, 0, 0},
+         new float[] {.11f, .11f, .11f, 0, 0},
+         new float[] {0, 0, 0, 1, 0},
+         new float[] {0, 0, 0, 0, 1}
+               });
+
+            //create some image attributes
+            ImageAttributes attributes = new ImageAttributes();
+
+            //set the color matrix attribute
+            attributes.SetColorMatrix(colorMatrix);
+
+            //draw the original image on the new image
+            //using the grayscale color matrix
+            g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
+               0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+
+            //dispose the Graphics object
+            g.Dispose();
+            return newBitmap;
+        }
+
+        public static int GetDiff2(Image img1, Image img2, int tolerance, int quality)
+        {
+            var bm1 = (Bitmap)img1;
+            var bm2 = (Bitmap)img2;
+
             var max_diff = 0;
             for (var x = 0; x < img1.Width; x++)
             {
@@ -216,7 +315,7 @@ namespace WebP.ConsoleApp
                         Math.Abs(color1.G - color2.G) +
                         Math.Abs(color1.B - color2.B);
 
-                    if (diff > tolerance) return diff;
+                    if (diff > tolerance && quality != 99) return diff;
                     if (diff > max_diff) max_diff = diff;
                 }
             }
@@ -313,17 +412,25 @@ namespace WebP.ConsoleApp
         public string OutputFolder => string.IsNullOrEmpty(_outputFolder) ? InputFolder : _outputFolder;
 
 
-        [Option('s', "starting-quality", Required = false, HelpText = "starting conversion quality, if not specified, then 40 percent quality is used. ex: -q 40")]
+        [Option('s', "starting-quality", Required = false, HelpText = "starting conversion quality, if not specified, then 40 percent quality is used. ex: -s 40")]
         public int _startingQuality { get; set; }
         public int StartingQuality => _startingQuality == 0 ? 40 : _startingQuality;
 
-        [Option('m', "max-parrallelism", Required = false, HelpText = "maximium parralellism, if not specified, then max value is used, if set to 0, then processor count x 10 is used. ex: -q 100")]
+        [Option('m', "max-parrallelism", Required = false, HelpText = "maximium parralellism, if not specified, then max value is used, if set to 0, then processor count x 10 is used. ex: -m 10")]
         public int? _maxParrallelism { get; set; }
         public int? MaxParrallelism => _maxParrallelism.HasValue && _maxParrallelism.Value == 0 ? Environment.ProcessorCount * 10 : _maxParrallelism;
 
-        [Option('t', "tolerant", Required = false, HelpText = "image difference tolerance, valid values are 0 - 765, default 100 . ex: -q 100")]
+        [Option('t', "tolerant", Required = false, HelpText = "image difference tolerance, valid values are 0 - 765, default 100 . ex: -t 100")]
         public int? _tolerance { get; set; }
         public int Tolerance => _tolerance.HasValue ? _tolerance.Value : 100;
+
+        [Option('l', "lossless", Required = false, HelpText = "when tolerance level could not be achieved, a false value would stop at 99% quality, a true value would stop at 100%, default is true. ex: -l false")]
+        public bool? _lossless { get; set; }
+        public bool Lossless => _lossless.HasValue ? _lossless.Value : true;
+
+        [Option('a', "alpha", Required = false, HelpText = "true to allow alpha channel, false to remove alpha and add white background. ex: -a false")]
+        public bool? _allowAlphaChannel { get; set; }
+        public bool AllowAlphaChannel => _allowAlphaChannel.HasValue ? _allowAlphaChannel.Value : false;
 
     }
 
